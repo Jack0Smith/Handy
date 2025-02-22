432c7a030a10a1a5c33fcb1384f7a56382e34b38
Hillskiller <hillskiller12@gmail.com>
2025-02-22 17:46:22 +0200

    test workspaceee3

--- Handy/backend/main.py
+++ Handy/backend/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1ad6eab4307fb73a7524d25899bbb3c0a7675021274b3e611daf594c11ba195
-size 584
+oid sha256:47a363b90ec5611c908509bded0588ebd87ae26eb673be644b32964e4e76b321
+size 508
